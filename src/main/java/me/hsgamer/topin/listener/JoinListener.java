5dbd3c51e32a221cc0e2c7cfea83fd006cbf7c23
hsgamer <huynhqtienvtag@gmail.com>
2020-07-09 18:55:36 +0700

    add only first-time players

--- src/main/java/me/hsgamer/topin/listener/JoinListener.java
+++ src/main/java/me/hsgamer/topin/listener/JoinListener.java
@@ -1,6 +1,7 @@
 package me.hsgamer.topin.listener;
 
 import me.hsgamer.topin.TopIn;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,6 +10,9 @@ public class JoinListener implements Listener {
 
   @EventHandler
   public void onJoin(PlayerJoinEvent event) {
-    TopIn.getInstance().getDataListManager().addNew(event.getPlayer().getUniqueId());
+    Player player = event.getPlayer();
+    if (!player.hasPlayedBefore()) {
+      TopIn.getInstance().getDataListManager().addNew(player.getUniqueId());
+    }
   }
 }

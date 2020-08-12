af1f4f332adfc7cad863f1feef3fe768de4d7445
HSGamer <huynhqtienvtag@gmail.com>
2020-08-12 21:23:36 +0700

    fix sign physics

--- src/main/java/me/hsgamer/topin/getter/sign/SignBreakListener.java
+++ src/main/java/me/hsgamer/topin/getter/sign/SignBreakListener.java
@@ -1,30 +1,20 @@
 package me.hsgamer.topin.getter.sign;
 
-import java.util.Arrays;
-import java.util.List;
 import me.hsgamer.hscore.bukkit.utils.MessageUtils;
 import me.hsgamer.topin.Permissions;
 import me.hsgamer.topin.config.MessageConfig;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 public final class SignBreakListener implements Listener {
 
-  private static final List<BlockFace> checkBlockFaceList = Arrays.asList(
-      BlockFace.UP,
-      BlockFace.DOWN,
-      BlockFace.WEST,
-      BlockFace.EAST,
-      BlockFace.NORTH,
-      BlockFace.SOUTH
-  );
   private final SignGetter signGetter;
 
   public SignBreakListener(SignGetter getter) {
@@ -34,12 +24,6 @@ public final class SignBreakListener implements Listener {
   @EventHandler
   public void onBreak(BlockBreakEvent event) {
     Block block = event.getBlock();
-    if (checkBlockFaceList.stream().anyMatch(
-        blockFace -> signGetter.containsSign(block.getRelative(blockFace).getLocation()))) {
-      event.setCancelled(true);
-      return;
-    }
-
     Location location = block.getLocation();
     Player player = event.getPlayer();
     if (signGetter.containsSign(location)) {
@@ -53,18 +37,19 @@ public final class SignBreakListener implements Listener {
   }
 
   @EventHandler
+  public void onPhysics(BlockPhysicsEvent event) {
+    if (signGetter.containsSign(event.getBlock().getLocation())) {
+      event.setCancelled(true);
+    }
+  }
+
+  @EventHandler
   public void onBlockExplode(BlockExplodeEvent event) {
-    event.blockList().removeIf(block ->
-        signGetter.containsSign(block.getLocation()) || checkBlockFaceList.stream().anyMatch(
-            blockFace -> signGetter.containsSign(block.getRelative(blockFace).getLocation()))
-    );
+    event.blockList().removeIf(block -> signGetter.containsSign(block.getLocation()));
   }
 
   @EventHandler
   public void onEntityExplode(EntityExplodeEvent event) {
-    event.blockList().removeIf(block ->
-        signGetter.containsSign(block.getLocation()) || checkBlockFaceList.stream().anyMatch(
-            blockFace -> signGetter.containsSign(block.getRelative(blockFace).getLocation()))
-    );
+    event.blockList().removeIf(block -> signGetter.containsSign(block.getLocation()));
   }
 }

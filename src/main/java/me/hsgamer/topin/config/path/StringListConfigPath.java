6eddf69df63dd32e63621cd0a990a25455321e9e
HSGamer <huynhqtienvtag@gmail.com>
2020-11-07 19:27:09 +0700

    StringListConfigPath as AdvancedConfigPath

--- src/main/java/me/hsgamer/topin/config/path/StringListConfigPath.java
+++ src/main/java/me/hsgamer/topin/config/path/StringListConfigPath.java
@@ -1,11 +1,14 @@
 package me.hsgamer.topin.config.path;
 
 import me.hsgamer.hscore.common.CommonUtils;
-import me.hsgamer.hscore.config.BaseConfigPath;
+import me.hsgamer.hscore.config.AdvancedConfigPath;
+import me.hsgamer.hscore.config.Config;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class StringListConfigPath extends BaseConfigPath<List<String>> {
+public class StringListConfigPath extends AdvancedConfigPath<Object, List<String>> {
 
     /**
      * Create a config path
@@ -14,6 +17,21 @@ public class StringListConfigPath extends BaseConfigPath<List<String>> {
      * @param def  the default value if it's not found
      */
     public StringListConfigPath(String path, List<String> def) {
-        super(path, def, o -> CommonUtils.createStringListFromObject(o, false));
+        super(path, def);
+    }
+
+    @Override
+    public @Nullable Object getFromConfig(@NotNull Config config) {
+        return config.getConfig().get(getPath());
+    }
+
+    @Override
+    public @Nullable List<String> convert(@NotNull Object rawValue) {
+        return CommonUtils.createStringListFromObject(rawValue, false);
+    }
+
+    @Override
+    public @Nullable Object convertToRaw(@NotNull List<String> value) {
+        return value;
     }
 }

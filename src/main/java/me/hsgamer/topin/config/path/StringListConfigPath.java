60cfb4ecd1e7473e4e3aaaa90c2bad17ec189150
Huynh Tien <huynhqtienvtag@gmail.com>
2020-12-21 21:39:47 +0700

    2.2 (#86)
    * BaseConfigPath
    
    * oops
    
    * Bump HSCore-bukkit-config from 2.0 to 2.2 (#75)
    
    Bumps HSCore-bukkit-config from 2.0 to 2.2.
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump HSCore-collections from 2.0 to 2.2 (#74)
    
    Bumps [HSCore-collections](https://github.com/HSGamer/HSCore) from 2.0 to 2.2.
    - [Release notes](https://github.com/HSGamer/HSCore/releases)
    - [Commits](https://github.com/HSGamer/HSCore/commits)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump HSCore-common from 2.0 to 2.2 (#73)
    
    Bumps [HSCore-common](https://github.com/HSGamer/HSCore) from 2.0 to 2.2.
    - [Release notes](https://github.com/HSGamer/HSCore/releases)
    - [Commits](https://github.com/HSGamer/HSCore/commits)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump HSCore-config-path from 2.0 to 2.2 (#72)
    
    Bumps HSCore-config-path from 2.0 to 2.2.
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump HSCore-bukkit-addon from 2.0 to 2.2 (#71)
    
    Bumps HSCore-bukkit-addon from 2.0 to 2.2.
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump HSCore-bukkit-utils from 2.0 to 2.2 (#70)
    
    Bumps HSCore-bukkit-utils from 2.0 to 2.2.
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump HSCore-checker-spigotmc from 2.0 to 2.2 (#69)
    
    Bumps HSCore-checker-spigotmc from 2.0 to 2.2.
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump HSCore-json from 2.0 to 2.2 (#68)
    
    Bumps [HSCore-json](https://github.com/HSGamer/HSCore) from 2.0 to 2.2.
    - [Release notes](https://github.com/HSGamer/HSCore/releases)
    - [Commits](https://github.com/HSGamer/HSCore/commits)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump HSCore-bukkit-command from 2.0 to 2.2 (#67)
    
    Bumps HSCore-bukkit-command from 2.0 to 2.2.
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * update HSCore to fix a config issue
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/main/java/me/hsgamer/topin/config/path/StringListConfigPath.java
+++ src/main/java/me/hsgamer/topin/config/path/StringListConfigPath.java
@@ -1,37 +1,20 @@
 package me.hsgamer.topin.config.path;
 
 import me.hsgamer.hscore.common.CollectionUtils;
-import me.hsgamer.hscore.config.AdvancedConfigPath;
-import me.hsgamer.hscore.config.Config;
+import me.hsgamer.hscore.config.BaseConfigPath;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class StringListConfigPath extends AdvancedConfigPath<Object, List<String>> {
-
+public class StringListConfigPath extends BaseConfigPath<List<String>> {
     /**
      * Create a config path
      *
-     * @param path the path to the value
-     * @param def  the default value if it's not found
+     * @param path          the path to the value
+     * @param def           the default value if it's not found
      */
-    public StringListConfigPath(String path, List<String> def) {
-        super(path, def);
-    }
-
-    @Override
-    public @Nullable Object getFromConfig(@NotNull Config config) {
-        return config.getConfig().get(getPath());
-    }
-
-    @Override
-    public @Nullable List<String> convert(@NotNull Object rawValue) {
-        return CollectionUtils.createStringListFromObject(rawValue, false);
-    }
-
-    @Override
-    public @Nullable Object convertToRaw(@NotNull List<String> value) {
-        return value;
+    public StringListConfigPath(@NotNull String path, @Nullable List<String> def) {
+        super(path, def, o -> CollectionUtils.createStringListFromObject(o, false));
     }
 }
